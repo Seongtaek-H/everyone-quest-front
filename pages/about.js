a38c16617ca8384663d3b540d38befb0abd5c18d
seongtaek <hst1993@hanmail.net>
2022-02-14 21:52:43 +0900

    start

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar.js";
+import Seo from "../components/Seo.js";
+
+export default function About() {
+  return (
+    <div>
+      <Seo title="about" />
+      <h1>About</h1>
+    </div>
+  );
+}
